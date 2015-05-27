e9c7f5f97195ca16774145911116c08e6d9393e2
taoajayi <ayyy>
2015-05-27 08:56:20 -0400

    DPProject
    stuff

--- UI.R
+++ UI.R
@@ -1,15 +1,10 @@
 library(shiny)
 
-# Define UI for dataset viewer application
 shinyUI(fluidPage(
   
-  # Application title
   titlePanel("Dataset Explorer"),
   
-  # Sidebar with controls to provide a caption, select a dataset,
-  # and specify the number of observations to view. Note that
-  # changes made to the caption in the textInput control are
-  # updated in the output area immediately as you type
+
   sidebarLayout(
     sidebarPanel(
       textInput("caption", "Caption:", "Data Summary"),
@@ -20,9 +15,7 @@ shinyUI(fluidPage(
       numericInput("obs", "Number of observations to view:", 10)
     ),
     
-    
-    # Show the caption, a summary of the dataset and an HTML 
-    # table with the requested number of observations
+
     mainPanel(
       h3(textOutput("caption", container = span)),
       
